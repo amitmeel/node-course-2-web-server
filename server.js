fe8631f2fc537a79e1b22ad1c2625035b10db55a
AmitMeel <amit.amitmeel.meel@gmail.com>
2018-07-02 14:21:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log+'\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+//app.use((req, res, next) => {
+//    res.render('maintanance.hbs');
+//});
+
+app.use(express.static(__dirname + '/public')); //register middleware
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+//handling routing
+app.get('/', (req, res) =>{
+  res.render('home.hbs',{
+      pageTitle: 'Home Page',
+      //currentYear: new Date().getFullYear(),
+      welcomeMessage: 'Welcome to Home Page'
+  }); 
+});
+
+app.get('/about', (req, res) => {
+   //res.send('About Page'); 
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        Error: 'unable to fetch data'
+    });
+});
+
+app.listen(3000, () =>{
+    console.log('server started on 3000');
+} );
